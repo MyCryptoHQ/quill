bf45d8cab21dbb69d0fe430a8112bca8f6302772
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-02 14:00:34 +0200

    Automatically add hex prefix if needed

--- src/api/signing.ts
+++ src/api/signing.ts
@@ -1,5 +1,6 @@
 import { Wallet } from "ethers";
 import { TransactionRequest } from "@ethersproject/abstract-provider"
+import { addHexPrefix } from "./util";
 
 // @todo Keystore
 
@@ -15,7 +16,7 @@ export const signWithPrivateKey = async (
   privateKey: string,
   tx: TransactionRequest
 ) => {
-  return sign(new Wallet(privateKey), tx);
+  return sign(new Wallet(addHexPrefix(privateKey)), tx);
 };
 
 export const sign = async (wallet: Wallet, tx: TransactionRequest) => {
--- src/api/util.ts
+++ src/api/util.ts
@@ -4,3 +4,7 @@ import { JsonRPCRequest } from "../types/JsonRPCRequest";
 export const makeTx = (request: JsonRPCRequest): TransactionRequest => {
   return request.params[0];
 };
+
+export const addHexPrefix = (str: string) => {
+  return str.slice(0, 2) === "0x" ? str : "0x" + str;
+};
