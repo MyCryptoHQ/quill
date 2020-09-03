7e8f120c6e56cd81e81ef474ac87e900c95e63e3
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-09-03 16:48:39 +0200

    Method should be declared before

--- src/api/signing.ts
+++ src/api/signing.ts
@@ -4,21 +4,21 @@ import { addHexPrefix } from './util';
 
 // @todo Keystore
 
-export const signWithMnemonic = async (
+export const sign = (wallet: Wallet, tx: TransactionRequest) => {
+  return wallet.signTransaction(tx);
+};
+
+export const signWithMnemonic = (
   mnemonicPhrase: string,
   dPath: string,
-  tx: TransactionRequest
+  tx: TransactionRequest,
 ) => {
   return sign(Wallet.fromMnemonic(mnemonicPhrase, dPath), tx);
 };
 
-export const signWithPrivateKey = async (
+export const signWithPrivateKey = (
   privateKey: string,
-  tx: TransactionRequest
+  tx: TransactionRequest,
 ) => {
   return sign(new Wallet(addHexPrefix(privateKey)), tx);
-};
-
-export const sign = async (wallet: Wallet, tx: TransactionRequest) => {
-  return await wallet.signTransaction(tx);
 };
