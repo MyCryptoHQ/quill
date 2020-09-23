0ce42a4039335f7706524862c47dab19bbdfb182
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-23 14:18:35 +0200

    Add Tray icon

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, screen, Tray } from 'electron';
+import path from 'path';
 
 import { runService as runSigningService } from '@api/sign';
 
 import { runAPI } from './api/ws';
 
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any;
+
+const HEIGHT = 450;
+const WIDTH = 300;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 // eslint-disable-next-line global-require
@@ -13,12 +18,18 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+let tray: Tray;
+let window: BrowserWindow;
+
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     backgroundColor: '#fbfbfb',
-    width: 1220,
-    height: 720,
+    width: WIDTH,
+    height: HEIGHT,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
     webPreferences: {
       devTools: true,
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
@@ -40,12 +51,92 @@ const createWindow = (): void => {
 
   // Run Signing Logic
   runSigningService();
+
+  window = mainWindow;
+};
+
+// Based on https://cdruc.com/positioning-electron-tray-apps-on-windows-taskbar/
+const getWindowPosition = () => {
+  const trayBounds = tray.getBounds();
+
+  // There may be more than one screen, so we need to figure out on which screen our tray icon lives.
+  const trayScreen = screen.getDisplayNearestPoint({
+    x: trayBounds.x,
+    y: trayBounds.y
+  });
+
+  // Now that we know the display, we can grab its bounds and its workspace area.
+  const { workArea } = trayScreen;
+  const screenBounds = trayScreen.bounds;
+
+  // TASKBAR LEFT
+  if (workArea.x > 0) {
+    // The workspace starts more on the right
+    return {
+      x: workArea.x,
+      y: workArea.height - HEIGHT
+    };
+  }
+
+  // TASKBAR TOP
+  if (workArea.y > 0) {
+    return {
+      x: Math.round(trayBounds.x + trayBounds.width / 2 - WIDTH / 2),
+      y: trayBounds.height
+    };
+  }
+
+  // TASKBAR RIGHT
+  // Here both workArea.y and workArea.x are 0 so we can no longer leverage them. We can use the workarea and display width though.
+  if (workArea.width < screenBounds.width) {
+    // The taskbar is either on the left or right, but since the LEFT case was handled above, we can be sure we're dealing with a right taskbar
+    return {
+      x: workArea.width - WIDTH,
+      y: screenBounds.height - HEIGHT
+    };
+  }
+
+  // TASKBAR BOTTOM
+  // Since all the other cases were handled, we can be sure we're dealing with a bottom taskbar
+  return {
+    x: Math.round(trayBounds.x + trayBounds.width / 2 - WIDTH / 2),
+    y: workArea.height - HEIGHT
+  };
+};
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  // Added because setPosition would sometimes squeeze the sizing
+  window.setSize(WIDTH, HEIGHT, false);
+  window.show();
+  window.focus();
+};
+
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide();
+  } else {
+    showWindow();
+  }
+};
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, 'favicon.png'));
+  tray.on('right-click', toggleWindow);
+  tray.on('double-click', toggleWindow);
+  tray.on('click', () => {
+    toggleWindow();
+  });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+  createTray();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- webpack_config/main.js
+++ webpack_config/main.js
@@ -1,7 +1,24 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const { merge } = require('webpack-merge');
 
 const common = require('./common');
 
 module.exports = merge(common, {
-  entry: './src/main.ts'
+  entry: './src/main.ts',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        path.resolve(
+          __dirname,
+          '../',
+          'src',
+          'app',
+          'assets',
+          'images',
+          'favicon.png',
+        ),
+      ],
+    }),
+  ],
 });
