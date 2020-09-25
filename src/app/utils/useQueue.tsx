40c2fe7a9df21a5eee02fd5e3239fab159395e63
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-25 11:12:51 +0200

    Add validation and test CI

--- src/app/utils/useQueue.tsx
+++ src/app/utils/useQueue.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const useQueue = <T,>() => {
   const [queue, setQueue] = useState<T[]>([]);
 
   const first = queue[0];
-  const length = queue.length;
+  const { length } = queue;
   const enqueue = (item: T) => setQueue((prevState) => [...prevState, item]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const dequeue = () => setQueue(([_, ...rest]) => [...rest]);
 
   return { first, length, enqueue, dequeue };
