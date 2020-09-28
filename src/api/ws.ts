576041eb6084a71aab76b01768522c4cb047b24a
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-28 14:53:54 +0200

    Remove reply function

--- src/api/ws.ts
+++ src/api/ws.ts
@@ -1,23 +1,19 @@
 import { WebContents } from 'electron';
 import WebSocket from 'ws';
 
-import { JsonRPCResponse } from '@types';
-
 import { handleRequest } from './api';
 
 export const runAPI = (webContents: WebContents) => {
   console.debug('Spinning up WS');
   const ws = new WebSocket.Server({ host: 'localhost', port: 8000 });
   ws.on('connection', (socket) => {
-    const reply = (response: JsonRPCResponse) =>
+    socket.on('message', async (data) => {
+      console.debug(data);
+      // Handle request in API and send response back when promise resolves.
+      const response = await handleRequest(data as string, webContents);
       socket.send(JSON.stringify(response), (err) => {
         if (err) console.error(err);
       });
-
-    socket.on('message', async (data) => {
-      console.debug(data);
-      const response = await handleRequest(data as string, webContents);
-      reply(response);
     });
   });
 };
