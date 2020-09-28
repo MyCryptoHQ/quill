427b587331ab84b260a3b6d3ee3703c5bed9b927
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-28 12:32:59 +0200

    Add validators test

---
+++ src/api/validators.spec.ts
@@ -0,0 +1,45 @@
+import { SUPPORTED_METHODS } from '@types';
+
+import { isValidRequest } from './validators';
+
+describe('isValidRequest', () => {
+  it('detects valid requests', () => {
+    const valid = isValidRequest({
+      id: 1,
+      method: SUPPORTED_METHODS.SIGN_TRANSACTION,
+      jsonrpc: '2.0',
+      params: [
+        {
+          to: '0x',
+          from: '0x',
+          nonce: '0x1',
+          gasLimit: '0x',
+          gasPrice: '0x',
+          data: '0x',
+          value: '0x',
+          chainId: 3
+        }
+      ]
+    });
+    expect(valid).toBe(true);
+  });
+
+  it('detects invalid requests', () => {
+    const valid = isValidRequest({
+      id: 1,
+      method: SUPPORTED_METHODS.SIGN_TRANSACTION,
+      jsonrpc: '2.0',
+      params: []
+    });
+    expect(valid).toBe(false);
+  });
+
+  it('detects invalid requests without params', () => {
+    const valid = isValidRequest({
+      id: 1,
+      method: SUPPORTED_METHODS.SIGN_TRANSACTION,
+      jsonrpc: '2.0'
+    } as any);
+    expect(valid).toBe(false);
+  });
+});
