14b63e2bfffc96b5e72e0e62bbb4dbd48c2a7d96
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-29 13:11:28 +0200

    Add logging library

--- src/api/ws.ts
+++ src/api/ws.ts
@@ -1,18 +1,19 @@
 import { WebContents } from 'electron';
+import log from 'electron-log';
 import WebSocket from 'ws';
 
 import { handleRequest } from './api';
 
 export const runAPI = (webContents: WebContents) => {
-  console.debug('Spinning up WS');
+  log.debug('Spinning up WS');
   const ws = new WebSocket.Server({ host: 'localhost', port: 8000 });
   ws.on('connection', (socket) => {
     socket.on('message', async (data) => {
-      console.debug(data);
+      log.debug(data);
       // Handle request in API and send response back when promise resolves.
       const response = await handleRequest(data as string, webContents);
       socket.send(JSON.stringify(response), (err) => {
-        if (err) console.error(err);
+        if (err) log.error(err);
       });
     });
   });
