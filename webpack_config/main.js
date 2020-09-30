038700c678dbfa93de9b33878dfea2fef619cdff
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-30 15:11:58 +0200

    Add Tray icon (#7)
    * Add Tray icon
    
    * Use correct icon
    
    * Fix position on launch
    
    * Add 32x32 favicon
    
    * Add ternary and smaller favicon

--- webpack_config/main.js
+++ webpack_config/main.js
@@ -1,7 +1,17 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const { merge } = require('webpack-merge');
 
 const common = require('./common');
 
 module.exports = merge(common, {
-  entry: './src/main.ts'
+  entry: './src/main.ts',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        path.resolve(__dirname, '../', 'src', 'app', 'assets', 'images', 'favicon.png'),
+        path.resolve(__dirname, '../', 'src', 'app', 'assets', 'images', 'icon.png')
+      ]
+    })
+  ]
 });
