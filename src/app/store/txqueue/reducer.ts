717859496a05d722c57d5496f391e26b398476e7
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-02 13:30:53 +0200

    Fix reducer issue

--- src/app/store/txqueue/reducer.ts
+++ src/app/store/txqueue/reducer.ts
@@ -5,10 +5,10 @@ import { dequeue, enqueue, INITIAL_STATE, TXQueueState } from './txqueue';
 export const reducer = createReducer<TXQueueState>(INITIAL_STATE, (builder) =>
   builder
     .addCase(enqueue, (state, action) => {
-      state = [...state, action.payload];
+      return [...state, action.payload];
     })
     .addCase(dequeue, (state) => {
       const [, ...queue] = state;
-      state = queue;
+      return queue;
     })
 );
