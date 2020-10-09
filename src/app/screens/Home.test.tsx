be2dc4686a33511969eed15e7799642ac6443b47
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-09 18:37:34 +0200

    Improved Home test

--- src/app/screens/Home.test.tsx
+++ src/app/screens/Home.test.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
 
 import { createStore } from '@app/store';
+import { ipcBridgeRenderer } from '@bridge';
+import { JsonRPCRequest } from '@types';
 
 import { Home } from '.';
+
 
 jest.mock('@bridge', () => ({
   ipcBridgeRenderer: {
     api: {
-      subscribeToRequests: () => {
+      subscribeToRequests: (callback: (request: JsonRPCRequest) => void) => {
+        const request = { id: 1, jsonrpc: '2.0', method: 'eth_signTransaction', params: [{}] };
+        callback(request);
+        callback({ id: 2, ...request });
         return () => true;
-      }
-    }
+      },
+      sendResponse: jest.fn()
+    },
+    crypto: { invoke: jest.fn() }
   }
 }));
 
@@ -29,8 +37,44 @@ function getComponent() {
 }
 
 describe('Home', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('renders', async () => {
     const { getByText } = getComponent();
     expect(getByText('Accept').textContent).toBeDefined();
+  });
+
+  it('can deny tx', async () => {
+    const { getByText } = getComponent();
+    const denyButton = getByText('Deny');
+    expect(denyButton.textContent).toBeDefined();
+
+    fireEvent.click(denyButton);
+
+    expect(ipcBridgeRenderer.api.sendResponse).toHaveBeenCalledWith(
+      expect.objectContaining({ id: 1, error: expect.objectContaining({ code: '-32000' }) })
+    );
+  });
+
+  it('can accept tx', async () => {
+    const { getByText, getByLabelText } = getComponent();
+    const acceptButton = getByText('Accept');
+    expect(acceptButton.textContent).toBeDefined();
+
+    const privkeyInput = getByLabelText('Private Key');
+    expect(privkeyInput).toBeDefined();
+    fireEvent.change(privkeyInput, { target: { value: 'privkey' } });
+
+    fireEvent.click(acceptButton);
+
+    expect(ipcBridgeRenderer.crypto.invoke).toHaveBeenCalled();
+
+    await waitFor(() =>
+      expect(ipcBridgeRenderer.api.sendResponse).toHaveBeenCalledWith(
+        expect.objectContaining({ id: 1 })
+      )
+    );
   });
 });
