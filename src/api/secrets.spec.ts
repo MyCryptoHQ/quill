aadc5dde2342ea70a9cc15d97915781b54bb42cb
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-12 18:03:28 +0200

    Add test for secrets API

---
+++ src/api/secrets.spec.ts
@@ -0,0 +1,64 @@
+import { ipcMain } from 'electron';
+import keytar from 'keytar';
+
+import { IPC_CHANNELS, KEYTAR_SERVICE } from '@config';
+import { SecretsRequestType, TUuid } from '@types';
+
+import { handleRequest, runService } from './secrets';
+
+jest.mock('electron', () => ({
+  ipcMain: {
+    handle: jest.fn()
+  }
+}));
+
+jest.mock('keytar', () => ({
+  setPassword: jest.fn(),
+  getPassword: jest.fn().mockImplementation(() => '4245ceb5ab93dd'),
+  deletePassword: jest.fn()
+}));
+
+const uuid = 'a259a13e-936b-5945-8c80-7f757e808507' as TUuid;
+const password = 'password';
+const privateKey = 'privkey';
+const encryptedPrivKey = '4245ceb5ab93dd';
+
+describe('handleRequest', () => {
+  it('SAVE_PRIVATE_KEY calls setPassword with encrypted privkey', async () => {
+    await handleRequest({
+      type: SecretsRequestType.SAVE_PRIVATE_KEY,
+      uuid,
+      password,
+      privateKey
+    });
+
+    expect(keytar.setPassword).toHaveBeenCalledWith(KEYTAR_SERVICE, uuid, encryptedPrivKey);
+  });
+
+  it('GET_PRIVATE_KEY returns decrypted private key', async () => {
+    const response = await handleRequest({
+      type: SecretsRequestType.GET_PRIVATE_KEY,
+      uuid,
+      password
+    });
+
+    expect(keytar.getPassword).toHaveBeenCalledWith(KEYTAR_SERVICE, uuid);
+    expect(response).toBe(privateKey);
+  });
+
+  it('DELETE_PRIVATE_KEY returns decrypted private key', async () => {
+    await handleRequest({
+      type: SecretsRequestType.DELETE_PRIVATE_KEY,
+      uuid
+    });
+
+    expect(keytar.deletePassword).toHaveBeenCalledWith(KEYTAR_SERVICE, uuid);
+  });
+});
+
+describe('runService', () => {
+  it('calls ipcMain handle', () => {
+    runService();
+    expect(ipcMain.handle).toHaveBeenCalledWith(IPC_CHANNELS.SECRETS, expect.any(Function));
+  });
+});
