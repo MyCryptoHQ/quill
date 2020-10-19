e2fc402233653ac7f73181d00832dbbbf3687a22
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-19 15:43:16 +0200

    Redirect from locked if logged in

--- src/app/screens/Locked.tsx
+++ src/app/screens/Locked.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useHistory } from 'react-router-dom';
+
+import { ROUTE_PATHS } from '@app/routing';
 import { useSelector } from '@app/store';
 
 import { Login } from './Login';
 import { NewUser } from './NewUser';
 
 export const Locked = () => {
-  const { newUser } = useSelector((state) => state.auth);
+  const { newUser, loggedIn } = useSelector((state) => state.auth);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (loggedIn) {
+      history.replace(ROUTE_PATHS.HOME);
+    }
+  }, [loggedIn]);
+
   if (newUser) {
     return <NewUser />;
   }
