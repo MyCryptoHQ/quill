e3c25083c2f4752cef8e8f9a6971ebe85d60924e
Frederik Bolding <frederik.bolding@gmail.com>
2021-03-08 12:16:27 +0100

    Reduce width slightly

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const SUPPORTED_METHODS = {
 export const WS_PORT = 8000;
 
 export const HEIGHT = 600;
-export const WIDTH = 500;
+export const WIDTH = 460;
 
 // @todo Change to another name?
 export const KEYTAR_SERVICE = 'MyCrypto Signer';
