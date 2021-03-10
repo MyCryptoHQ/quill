abce9bb4aa19ffd43d14860331261abc90b7b62a
Frederik Bolding <frederik.bolding@gmail.com>
2021-03-10 15:44:09 +0100

    Simplify bigify and add tests

---
+++ src/utils/bigify.spec.ts
@@ -0,0 +1,20 @@
+import { BigNumber as BigNumberish } from '@ethersproject/bignumber';
+
+import { bigify } from './bigify';
+
+describe('bigify', () => {
+  it('supports Ethers BN', () => {
+    const input = BigNumberish.from('999999999999999999999');
+    expect(bigify(input).toString(16)).toEqual(input.toHexString().slice(2));
+  });
+
+  it('supports bigint', () => {
+    const input = BigInt(999999999999999999999);
+    expect(bigify(input).toString(16)).toEqual(input.toString(16));
+  });
+
+  it('supports bytes', () => {
+    const input = Uint8Array.from([255, 255]);
+    expect(bigify(input).toString(16)).toEqual('ffff');
+  });
+});
--- src/utils/bigify.ts
+++ src/utils/bigify.ts
@@ -1,4 +1,4 @@
-import { BigNumber as BigNumberish } from '@ethersproject/bignumber';
+import { BigNumber as BigNumberish, isBigNumberish } from '@ethersproject/bignumber/lib/bignumber';
 import BigNumber from 'bignumber.js';
 
 export type Bigish = BigNumber;
@@ -7,12 +7,10 @@ export const bigify = (
   v: BigNumber.Value | BigNumberish | bigint | ArrayLike<number>
 ): BigNumber => {
   BigNumber.config({ DECIMAL_PLACES: 18, EXPONENTIAL_AT: 1e9 });
-  if (BigNumberish.isBigNumber(v) && 'toHexString' in v) {
-    return new BigNumber(v.toHexString());
+  if (isBigNumberish(v)) {
+    return new BigNumber(BigNumberish.from(v).toHexString());
   } else if (typeof v === 'bigint') {
     return new BigNumber(v.toString(16), 16);
-  } else if (Object.prototype.hasOwnProperty.call(v, 'length') && typeof v !== 'string') {
-    return new BigNumber(Buffer.from(v as ArrayLike<number>).toString('hex'));
   } else {
     return new BigNumber(v as BigNumber.Value);
   }
