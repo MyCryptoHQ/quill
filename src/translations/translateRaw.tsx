9782aef4b5dbb3cfe7d655aa4c03134611ebc818
Frederik Bolding <frederik.bolding@gmail.com>
2021-03-11 17:00:38 +0100

    Add tests

--- src/translations/translateRaw.tsx
+++ src/translations/translateRaw.tsx
@@ -1,3 +1,5 @@
+import React, { ReactNode } from 'react';
+
 import en from './lang/en.json';
 
 const fallbackLanguage = 'en';
@@ -70,3 +72,79 @@ export function translateRaw(key: string, variables?: { [name: string]: string }
   }
   return translatedString;
 }
+
+export const Trans = ({
+  id,
+  variables
+}: {
+  id: string;
+  variables?: { [name: string]: () => string | React.ReactNode };
+}) => {
+  const settings = { language: 'en' };
+  const language = settings.language || fallbackLanguage;
+  let tString =
+    (repository[language] && repository[language][id]) || repository[fallbackLanguage][id] || id;
+
+  const uniqueId = ((counter) => () => `${++counter}`)(0);
+
+  if (!variables) {
+    return <>{tString}</>;
+  } else {
+    const keys = Object.keys(variables);
+
+    const variablesComponents: ReactNode[] = [];
+
+    /**
+     * Replace all variables with tags. I.E. $variable1 with <T1>
+     */
+    keys.forEach((key, index) => {
+      const tStringSplit = tString.split(new RegExp(`\\${key}`));
+      if (tStringSplit.length) {
+        tString = tStringSplit.join(`<T${index}/>`);
+        const value = variables[keys[index]];
+        variablesComponents.push(value);
+      }
+    });
+
+    const nextTagMatch = new RegExp('<T\\d+/>');
+
+    /**
+     * Replaces all <T{id}> with actual components from variables, and creates a components list
+     * @param rest Rest of string to parse
+     * @param components Components array of parsed string
+     */
+    const resolveComponents = (rest: string, components: ReactNode[] = []): ReactNode[] => {
+      if (rest.length === 0) {
+        return components;
+      }
+
+      const nextMatch = rest.match(nextTagMatch);
+      if (nextMatch && nextMatch.length) {
+        const resolvedComponentIndex = nextMatch[0].replace('<T', '').replace('/>', '');
+        const resolvedComponentIndexNumber = parseInt(resolvedComponentIndex, 10);
+
+        const matchSplit = [
+          rest.substring(0, nextMatch.index!),
+          rest.substring(nextMatch.index! + 4 + resolvedComponentIndex.length)
+        ];
+
+        return resolveComponents(matchSplit[1], [
+          ...components,
+          <React.Fragment key={uniqueId()}>{matchSplit[0]}</React.Fragment>,
+          <React.Fragment key={uniqueId()}>
+            {typeof variablesComponents[resolvedComponentIndexNumber] === 'function'
+              ? (variablesComponents[resolvedComponentIndexNumber] as () => unknown)()
+              : variablesComponents[resolvedComponentIndexNumber]}
+          </React.Fragment>
+        ]);
+      }
+
+      return resolveComponents('', [
+        ...components,
+        <React.Fragment key={uniqueId()}>{rest}</React.Fragment>
+      ]);
+    };
+
+    return <>{resolveComponents(tString)}</>;
+  }
+};
