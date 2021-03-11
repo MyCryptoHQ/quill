23facc8dea72598b88f775335839d8b0ee832691
Frederik Bolding <frederik.bolding@gmail.com>
2021-03-11 16:28:51 +0100

    Add label copy

--- src/app/components/Account.tsx
+++ src/app/components/Account.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Box, BoxProps } from '@app/components';
+import { translateRaw } from '@translations';
 import { TAddress } from '@types';
 import { truncate } from '@utils';
 
@@ -16,7 +17,7 @@ export const Account = ({
   <Box bg="GREY_LIGHTEST" p="10px" variant="rowAlign" {...props}>
     <Blockie height="32px" width="32px" address={address} />
     <Box pl="1">
-      <Body fontSize="3">{label ? label : 'No Label'}</Body>
+      <Body fontSize="3">{label ? label : translateRaw('NO_LABEL')}</Body>
       <Body>{shouldTruncate ? truncate(address) : address}</Body>
     </Box>
   </Box>
