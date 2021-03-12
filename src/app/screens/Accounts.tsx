4f1c53f229b6d4a09498bfcc64480b57ad27dd25
Frederik Bolding <frederik.bolding@gmail.com>
2021-03-12 11:20:53 +0100

    Remove arrow function

--- src/app/screens/Accounts.tsx
+++ src/app/screens/Accounts.tsx
@@ -4,19 +4,28 @@ import { useSelector } from 'react-redux';
 
 import { getAccounts, removeAccount, useDispatch } from '@app/store';
 import { translateRaw } from '@translations';
+import { IAccount } from '@types';
+
+const DeleteAccountButton = ({ account }: { account: IAccount }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(removeAccount(account));
+
+  return (
+    <button data-testid={`delete-${account.address}`} onClick={handleDelete}>
+      {translateRaw('DELETE')}
+    </button>
+  );
+};
 
 export const Accounts = () => {
   const accounts = useSelector(getAccounts);
-  const dispatch = useDispatch();
 
   return (
     <>
       {accounts.map((a) => (
         <div key={a.uuid}>
           {a.address}
-          <button data-testid={`delete-${a.address}`} onClick={() => dispatch(removeAccount(a))}>
-            {translateRaw('DELETE')}
-          </button>
+          <DeleteAccountButton account={a} />
         </div>
       ))}
     </>
