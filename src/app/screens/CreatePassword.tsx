60e3ea58906ff633d6c6ead4dcd819fb1b7ea874
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-18 12:40:48 +0100

    Use standardised margins

--- src/app/screens/CreatePassword.tsx
+++ src/app/screens/CreatePassword.tsx
@@ -63,17 +63,17 @@ export const CreatePassword = () => {
 
   return (
     <Box>
-      <Heading fontSize="24px" lineHeight="150%" mt="24px" mb="8px" textAlign="center">
+      <Heading fontSize="24px" lineHeight="150%" mt="4" mb="2" textAlign="center">
         Create a Password
       </Heading>
       <Body>{translateRaw('CREATE_PASSWORD_DESCRIPTION_1')}</Body>
       <form onSubmit={handleSubmit}>
-        <Box width="100%" mt="16px">
+        <Box width="100%" mt="3">
           <Label htmlFor="password">{translateRaw('ENTER_PASSWORD')}</Label>
           <FormInput id="password" name="password" type="password" form={form} />
           <FormError name="password" form={form} />
         </Box>
-        <Box width="100%" mt="16px">
+        <Box width="100%" mt="3">
           <Label htmlFor="passwordConfirmation">{translateRaw('CONFIRM_PASSWORD')}</Label>
           <FormInput
             id="passwordConfirmation"
@@ -84,7 +84,7 @@ export const CreatePassword = () => {
           <FormError name="passwordConfirmation" form={form} />
         </Box>
         <Flex mt="20px">
-          <Image src={warning} width="20px" height="20px" minWidth="20px" alt="Warning" mr="6px" />
+          <Image src={warning} width="20px" height="20px" minWidth="20px" alt="Warning" mr="2" />
           <Body>
             {translate('CREATE_PASSWORD_DESCRIPTION_2', {
               $link: getKBHelpArticle(KB_HELP_ARTICLE.HOW_TO_BACKUP)
--- src/app/screens/NewUser.tsx
+++ src/app/screens/NewUser.tsx
@@ -8,16 +8,16 @@ import { translate, translateRaw } from '@translations';
 export const NewUser = () => (
   <Flex height="100%" flexDirection="column" variant="columnCenter">
     <Logo width="100px" height="100px" />
-    <Heading fontSize="30px" lineHeight="48px" mt="12px" mb="9px">
+    <Heading fontSize="30px" lineHeight="48px" mt="3" mb="2">
       {translateRaw('NEW_USER_HEADER')}
     </Heading>
-    <Body lineHeight="24px" mb="24px">
+    <Body lineHeight="24px" mb="4">
       {translateRaw('NEW_USER_DESCRIPTION_1')}
     </Body>
-    <Body lineHeight="24px" mb="24px">
+    <Body lineHeight="24px" mb="4">
       {translateRaw('NEW_USER_DESCRIPTION_2')}
     </Body>
-    <Body lineHeight="24px" mb="16px">
+    <Body lineHeight="24px" mb="3">
       {translate('NEW_USER_DESCRIPTION_3')}
     </Body>
     <LinkApp href={ROUTE_PATHS.CREATE_PASSWORD} width="100%">
