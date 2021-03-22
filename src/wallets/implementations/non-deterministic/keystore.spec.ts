b56de686073c5a8656974e5a2b3b46cdf9168be0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-22 19:52:07 +0100

    Add support for more keystore file types (#63)
    * Add support for more keystore file types
    
    * Fix issue with Jest
    
    * Add invalid keystore vectors
    
    * Add types for V3 keystores

--- src/wallets/implementations/non-deterministic/keystore.spec.ts
+++ src/wallets/implementations/non-deterministic/keystore.spec.ts
@@ -1,10 +1,48 @@
-import { fKeystore, fKeystorePassword, fPrivateKey } from '@fixtures';
+/**
+ * @jest-environment node
+ */
+
+import { fKeystoreVectors } from '@fixtures';
 
 import { Keystore } from './keystore';
 
 describe('Keystore', () => {
-  it('decrypts a keystore file with a password', async () => {
-    const wallet = new Keystore(fKeystore, fKeystorePassword);
-    await expect(wallet.getPrivateKey()).resolves.toBe(fPrivateKey);
-  });
+  it.concurrent.each(fKeystoreVectors.presale)(
+    'decrypts a presale keystore file with a password',
+    async ({ json, password, privateKey }) => {
+      const wallet = new Keystore(JSON.stringify(json), password);
+      await expect(wallet.getPrivateKey()).resolves.toBe(privateKey);
+    }
+  );
+
+  it.concurrent.each(fKeystoreVectors.v1)(
+    'decrypts a v1 keystore file with a password',
+    async ({ json, password, privateKey }) => {
+      const wallet = new Keystore(JSON.stringify(json), password);
+      await expect(wallet.getPrivateKey()).resolves.toBe(privateKey);
+    }
+  );
+
+  it.concurrent.each(fKeystoreVectors.v2)(
+    'decrypts a v2 keystore file',
+    async ({ json, privateKey }) => {
+      const wallet = new Keystore(JSON.stringify(json), undefined);
+      await expect(wallet.getPrivateKey()).resolves.toBe(privateKey);
+    }
+  );
+
+  it.concurrent.each(fKeystoreVectors.v3)(
+    'decrypts a v3 keystore file with a password',
+    async ({ json, password, privateKey }) => {
+      const wallet = new Keystore(JSON.stringify(json), password);
+      await expect(wallet.getPrivateKey()).resolves.toBe(privateKey);
+    }
+  );
+
+  it.concurrent.each(fKeystoreVectors.invalid)(
+    'throws on invalid keystore files',
+    async ({ json, password }) => {
+      expect(() => new Keystore(JSON.stringify(json), password)).toThrow();
+    }
+  );
 });
