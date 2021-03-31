ca7b4291375fd9e8761416d60630a96dbcc3b990
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-31 14:22:53 +0200

    Fix forgot password page

--- src/app/screens/ForgotPassword.tsx
+++ src/app/screens/ForgotPassword.tsx
@@ -4,7 +4,7 @@ import { push } from 'connected-react-router';
 
 import warning from '@assets/icons/circle-error.svg';
 import { ipcBridgeRenderer } from '@bridge';
-import { Body, Box, Button, Flex, Heading, Image } from '@components';
+import { Body, Button, Container, Flex, Heading, Image, PanelBottom } from '@components';
 import { ROUTE_PATHS } from '@routing';
 import { setNewUser, useDispatch } from '@store';
 import { translateRaw } from '@translations';
@@ -24,24 +24,24 @@ export const ForgotPassword = () => {
   };
 
   return (
-    <Flex variant="columnAlign" height="100%">
-      <Flex variant="columnCenter" mt="4" maxWidth="300px" mx="auto">
-        <Image alt="Warning" src={warning} width="100px" height="100px" />
-        <Heading fontSize="4" lineHeight="24px" mt="4" textAlign="center">
-          {translateRaw('RESET_HEADING')}
-        </Heading>
-        <Body variant="muted" mt="2" textAlign="center">
-          {translateRaw('RESET_DESCRIPTION')}
-        </Body>
-      </Flex>
-      <Box mt="auto" mb="3">
-        <Button mt="3" onClick={handleBack}>
-          {translateRaw('RESET_CANCEL')}
-        </Button>
+    <>
+      <Container>
+        <Flex variant="columnCenter" maxWidth="300px" mx="auto">
+          <Image alt="Warning" src={warning} width="100px" height="100px" />
+          <Heading fontSize="4" lineHeight="24px" mt="4" textAlign="center">
+            {translateRaw('RESET_HEADING')}
+          </Heading>
+          <Body variant="muted" mt="2" textAlign="center">
+            {translateRaw('RESET_DESCRIPTION')}
+          </Body>
+        </Flex>
+      </Container>
+      <PanelBottom variant="clear">
+        <Button onClick={handleBack}>{translateRaw('RESET_CANCEL')}</Button>
         <Button variant="inverted" mt="3" onClick={handleReset}>
           {translateRaw('RESET_CONFIRM')}
         </Button>
-      </Box>
-    </Flex>
+      </PanelBottom>
+    </>
   );
 };
