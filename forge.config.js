f416b9ff71450c656ff0d7375aebc4ce6cd461f2
Guillaume Roux <guillaumeroux123@gmail.com>
2021-04-07 19:34:04 +0200

    Add App signing and notorization

---
+++ forge.config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  packagerConfig: {
+    osxSign: {
+      identity: 'Developer ID Application: Felix Rieseberg (LT94ZKYDCJ)',
+      'hardened-runtime': true,
+      'gatekeeper-assess': false,
+      entitlements: 'entitlements.plist',
+      'entitlements-inherit': 'entitlements.plist',
+      'signature-flags': 'library'
+    },
+    osxNotarize: {
+      appleId: process.env['APPLE_ID'],
+      appleIdPassword: process.env['APPLE_ID_PASSWORD']
+    },
+    packageManager: 'yarn'
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'signer'
+      }
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        port: 3001,
+        mainConfig: './webpack_config/main.js',
+        renderer: {
+          config: './webpack_config/renderer.js',
+          entryPoints: [
+            {
+              html: './src/app/index.html',
+              js: './src/app/index.tsx',
+              name: 'main_window',
+              preload: {
+                js: './src/preload.ts'
+              }
+            }
+          ]
+        }
+      }
+    ]
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'MyCryptoHQ',
+          name: 'signer'
+        },
+        prerelease: true
+      }
+    }
+  ]
+};
