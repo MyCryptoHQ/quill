cc2a352f95d5fc0fb20aeac8be0262cb9fa638a3
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-20 11:22:33 +0200

    Add some tests

--- src/app/store/reducer.test.ts
+++ src/app/store/reducer.test.ts
@@ -1,4 +1,5 @@
 import { createHashHistory } from 'history';
+import { REHYDRATE } from 'redux-persist';
 
 import { logout } from '@common/store';
 import { fAccounts, fRequestOrigin, fTxRequest } from '@fixtures';
@@ -35,5 +36,20 @@ describe('rootReducer', () => {
     };
 
     expect(rootReducer(modifiedState, logout())).toStrictEqual(initialState);
+  });
+
+  it('REHYDRATE sets state directly', () => {
+    const rootReducer = createRootReducer(createHashHistory());
+    const state = rootReducer(undefined, {
+      type: 'foo'
+    });
+
+    expect(
+      rootReducer(state, {
+        type: REHYDRATE,
+        payload: { ...state.accounts, accounts: fAccounts },
+        key: 'accounts'
+      })
+    ).toStrictEqual({ ...state, accounts: { ...state.accounts, accounts: fAccounts } });
   });
 });
