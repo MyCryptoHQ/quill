f2659d958ab77e418cac90829e33c8a3490d6153
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-04-26 17:54:14 +0200

    Move worker webpack config to separate file

---
+++ webpack_config/worker.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const common = require('./common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: {
+    worker: './src/crypto/crypto.worker.ts'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../.webpack/main')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              '@babel/preset-typescript',
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: 'current'
+                  },
+                  modules: false
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+});
