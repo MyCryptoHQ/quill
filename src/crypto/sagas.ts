d0254c2f2d14364e5ba1c9bf83837015d6297d0b
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-26 11:09:52 +0200

    Add missing saga

--- src/crypto/sagas.ts
+++ src/crypto/sagas.ts
@@ -4,12 +4,14 @@ import { handshakeSaga, Process } from '@common/store';
 import type { ReduxIPC } from '@types';
 
 import { accountsSaga } from './accounts.sagas';
+import { authSaga } from './auth.sagas';
 import { signingSaga } from './signing.sagas';
 
 export default function* rootSaga(ipc: ReduxIPC) {
   yield all([
     handshakeSaga({ [Process.Main]: ipc }, Process.Crypto),
     signingSaga(),
-    accountsSaga()
+    accountsSaga(),
+    authSaga()
   ]);
 }
