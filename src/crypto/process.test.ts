ac50c5d9d529573fabeb942c204d48fd7adaf41b
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-26 14:46:04 +0200

    Remove unneeded test

--- src/crypto/process.test.ts
+++ src/crypto/process.test.ts
@@ -2,17 +2,13 @@ import { fork } from 'child_process';
 
 import { createCryptoProcess } from './process';
 
-const mockOn = jest.fn();
 jest.mock('child_process', () => ({
-  fork: jest.fn().mockImplementation(() => ({
-    on: mockOn
-  }))
+  fork: jest.fn().mockImplementation(() => ({}))
 }));
 
 describe('createCryptoProcess', () => {
   it('forks a child process', () => {
     expect(() => createCryptoProcess()).not.toThrow();
     expect(fork).toHaveBeenCalled();
-    expect(mockOn).toHaveBeenCalled();
   });
 });
