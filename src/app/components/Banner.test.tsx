832029c51283f0606fe74cc3c6205cd660a26cc8
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-05-12 15:11:24 +0200

    Fix banner test

--- src/app/components/Banner.test.tsx
+++ src/app/components/Banner.test.tsx
@@ -1,19 +1,30 @@
 import { fireEvent, render } from '@testing-library/react';
+import type { ComponentProps } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import { Banner } from '@components/Banner';
+import { theme } from '@theme';
+
+const getComponent = (props: ComponentProps<typeof Banner>) => {
+  return render(
+    <ThemeProvider theme={theme}>
+      <Banner {...props} />
+    </ThemeProvider>
+  );
+};
 
 describe('Banner', () => {
   it('renders the label', () => {
-    const { getByText } = render(<Banner type="info" label="Foo bar" />);
+    const { getByText } = getComponent({ type: 'info', label: 'Foo bar' });
     expect(() => getByText('Foo bar')).not.toThrow();
   });
 
   it('renders the children when extended', () => {
-    const { getByText, getByTestId } = render(
-      <Banner type="info" label="Foo bar">
-        Baz qux
-      </Banner>
-    );
+    const { getByText, getByTestId } = getComponent({
+      type: 'info',
+      label: 'Foo bar',
+      children: 'Baz qux'
+    });
     expect(() => getByText('Baz qux')).toThrow();
 
     const toggle = getByTestId('banner-toggle');
@@ -23,16 +34,17 @@ describe('Banner', () => {
   });
 
   it('renders the children when extended by default', () => {
-    const { getByText } = render(
-      <Banner type="info" label="Foo bar" extended={true}>
-        Baz qux
-      </Banner>
-    );
+    const { getByText } = getComponent({
+      type: 'info',
+      label: 'Foo bar',
+      children: 'Baz qux',
+      extended: true
+    });
     expect(() => getByText('Baz qux')).not.toThrow();
   });
 
   it('renders an inner banner', () => {
-    const { getByText } = render(<Banner type="info" label="Foo bar" banner="Baz qux" />);
+    const { getByText } = getComponent({ type: 'info', label: 'Foo bar', banner: 'Baz qux' });
     expect(() => getByText('Baz qux')).not.toThrow();
   });
 });
