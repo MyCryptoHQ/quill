c496ed6a315df610635f65c989936b9b94c78b4e
Frederik Bolding <frederik.bolding@gmail.com>
2021-05-17 15:32:15 +0200

    Add tests

---
+++ src/api/store/userAttention.sagas.test.ts
@@ -0,0 +1,47 @@
+import type { BrowserWindow } from 'electron';
+import { expectSaga } from 'redux-saga-test-plan';
+
+import { requestPermission } from '@common/store';
+import { fPermission, fRequestOrigin, fTxRequest } from '@fixtures';
+import { showWindowOnTop } from '@window';
+
+import { userAttentionSaga } from './userAttention.sagas';
+import { requestSignTransaction } from './ws.slice';
+
+const request = { origin: fRequestOrigin, request: fTxRequest };
+
+const mockWindow = ({
+  hide: jest.fn(),
+  show: jest.fn(),
+  focus: jest.fn(),
+  setAlwaysOnTop: jest.fn(),
+  setVisibleOnAllWorkspaces: jest.fn()
+} as unknown) as BrowserWindow;
+
+describe('userAttentionSaga', () => {
+  it('calls showWindowOnTop when new transactions are requested signed', async () => {
+    await expectSaga(userAttentionSaga, mockWindow)
+      .dispatch(requestSignTransaction(request))
+      .call(showWindowOnTop, mockWindow)
+      .silentRun();
+
+    expect(mockWindow.hide).toHaveBeenCalled();
+    expect(mockWindow.show).toHaveBeenCalled();
+    expect(mockWindow.focus).toHaveBeenCalled();
+    expect(mockWindow.setAlwaysOnTop).toHaveBeenCalled();
+    expect(mockWindow.setVisibleOnAllWorkspaces).toHaveBeenCalled();
+  });
+
+  it('calls showWindowOnTop when permissions are requested', async () => {
+    await expectSaga(userAttentionSaga, mockWindow)
+      .dispatch(requestPermission(fPermission))
+      .call(showWindowOnTop, mockWindow)
+      .silentRun();
+
+    expect(mockWindow.hide).toHaveBeenCalled();
+    expect(mockWindow.show).toHaveBeenCalled();
+    expect(mockWindow.focus).toHaveBeenCalled();
+    expect(mockWindow.setAlwaysOnTop).toHaveBeenCalled();
+    expect(mockWindow.setVisibleOnAllWorkspaces).toHaveBeenCalled();
+  });
+});
