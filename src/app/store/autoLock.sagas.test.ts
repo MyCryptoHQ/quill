511b204bd926e503965874b1850e15b6e46c9ec3
Frederik Bolding <frederik.bolding@gmail.com>
2021-05-25 15:14:13 +0200

    Basic auto lock (#123)
    * Add basic auto lock
    
    * Fix a few bugs
    
    * Rewrite tests
    
    * Remove unused code
    
    * Restructure tests and add test for subscribe

---
+++ src/app/store/autoLock.sagas.test.ts
@@ -0,0 +1,58 @@
+import { testSaga } from 'redux-saga-test-plan';
+import { call, take } from 'redux-saga/effects';
+
+import { getLoggedIn, logout } from '@common/store';
+import { AUTO_LOCK_TIMEOUT } from '@config';
+
+import { autoLockWorker, delayedLock, subscribe } from './autoLock.sagas';
+
+describe('autoLockSaga', () => {
+  it('starts race between delayedLock and input', async () => {
+    jest.spyOn(window, 'addEventListener');
+    const channel = subscribe();
+    testSaga(autoLockWorker)
+      .next()
+      .call(subscribe)
+      .next(channel)
+      .race({ task: call(delayedLock), cancel: take(channel) })
+      .finish()
+      .isDone();
+
+    expect(window.addEventListener).toHaveBeenCalled();
+  });
+});
+
+describe('delayedLock', () => {
+  it('logs out if not cancelled', async () => {
+    testSaga(delayedLock)
+      .next()
+      .delay(AUTO_LOCK_TIMEOUT)
+      .next()
+      .select(getLoggedIn)
+      .next(true)
+      .put(logout())
+      .next()
+      .isDone();
+  });
+
+  it('doesnt log out if already logged out', async () => {
+    testSaga(delayedLock)
+      .next()
+      .delay(AUTO_LOCK_TIMEOUT)
+      .next()
+      .select(getLoggedIn)
+      .next(false)
+      .isDone();
+  });
+});
+
+describe('subscribe', () => {
+  jest.spyOn(window, 'addEventListener');
+  jest.spyOn(window, 'removeEventListener');
+  it('creates an event channel and tears it down', () => {
+    const channel = subscribe();
+    expect(window.addEventListener).toHaveBeenCalled();
+    channel.close();
+    expect(window.removeEventListener).toHaveBeenCalled();
+  });
+});
---
+++ src/app/store/autoLock.sagas.ts
@@ -0,0 +1,41 @@
+import { eventChannel } from 'redux-saga';
+import { all, call, delay, put, race, select, take } from 'redux-saga/effects';
+
+import { getLoggedIn, logout } from '@common/store';
+import { AUTO_LOCK_TIMEOUT } from '@config';
+
+export function* autoLockSaga() {
+  yield all([autoLockWorker()]);
+}
+
+export function* autoLockWorker() {
+  const channel = yield call(subscribe);
+  while (true) {
+    yield race({
+      task: call(delayedLock),
+      cancel: take(channel)
+    });
+  }
+}
+
+export function* delayedLock() {
+  yield delay(AUTO_LOCK_TIMEOUT);
+  const isLoggedIn: boolean = yield select(getLoggedIn);
+  if (isLoggedIn) {
+    yield put(logout());
+  }
+}
+
+export const subscribe = () => {
+  return eventChannel((emitter) => {
+    const events = ['mousemove', 'keydown'];
+
+    const listener = (event: Event) => emitter(event);
+
+    events.forEach((e) => window.addEventListener(e, listener));
+
+    return () => {
+      events.forEach((e) => window.removeEventListener(e, listener));
+    };
+  });
+};
