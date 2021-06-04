1355536e42cac35766a67dd47eac5f817f175d3b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-06-04 14:15:31 +0200

    Fix e2e path

--- packages/signer/tests/e2e/setupElectron.ts
+++ packages/signer/tests/e2e/setupElectron.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { Application } from 'spectron';
 
-const windowsPath = path.join(__dirname, '../out/signer-win32-x64/signer.exe');
+const windowsPath = path.join(__dirname, '../../out/signer-win32-x64/signer.exe');
 // @todo verify
-const macPath = path.join(__dirname, '../out/signer-macosx-x64/signer');
-const linuxPath = path.join(__dirname, '../out/signer-linux-x64/signer');
+const macPath = path.join(__dirname, '../../out/signer-macosx-x64/signer');
+const linuxPath = path.join(__dirname, '../../out/signer-linux-x64/signer');
 
 const appPath = (() => {
   switch (process.platform) {
