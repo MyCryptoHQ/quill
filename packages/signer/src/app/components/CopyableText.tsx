c150e5290b4fd73ac3cb6ff9859e26e5ec70b598
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-06-08 15:42:14 +0200

    Change icon when copying

--- packages/signer/src/app/components/CopyableText.tsx
+++ packages/signer/src/app/components/CopyableText.tsx
@@ -1,5 +1,7 @@
 import { Body, Box, Flex, Image } from '@mycrypto/ui';
+import { useEffect, useState } from 'react';
 
+import checkmark from '@assets/icons/checkmark.svg';
 import copy from '@assets/icons/copy.svg';
 
 export interface CopyableTextProps {
@@ -7,7 +9,22 @@ export interface CopyableTextProps {
 }
 
 export const CopyableText = ({ children }: CopyableTextProps) => {
-  const handleCopy = () => navigator.clipboard.writeText(children);
+  const [icon, setIcon] = useState(copy);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(children);
+    setIcon(checkmark);
+  };
+
+  useEffect(() => {
+    if (icon === checkmark) {
+      const timeout = setTimeout(() => {
+        setIcon(copy);
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [icon]);
 
   return (
     <Flex variant="horizontal-align" backgroundColor="badge.muted" p="3">
@@ -16,7 +33,7 @@ export const CopyableText = ({ children }: CopyableTextProps) => {
       </Box>
       <Box ml="3" minWidth="13px">
         <Image
-          src={copy}
+          src={icon}
           width="13px"
           height="13px"
           sx={{ cursor: 'pointer' }}
