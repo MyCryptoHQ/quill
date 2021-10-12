ca08a117f5b43375181548c68d3332e2695eb30a
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-12 13:31:41 +0200

    Show multiple accounts when adding

--- packages/signer/src/app/screens/AddAccount/AddAccountBackup.tsx
+++ packages/signer/src/app/screens/AddAccount/AddAccountBackup.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Link } from 'rebass/styled-components';
 
 import type { IFlowComponentProps } from '@components';
-import { Box, Checkbox, Container, Panel, PanelBottom, PaperWallet } from '@components';
+import { Box, Checkbox, Panel, PanelBottom, PaperWallet, ScrollableContainer } from '@components';
 import { getKBHelpArticle, KB_HELP_ARTICLE } from '@config';
 import { useDispatch, useSelector } from '@store';
 import { translate } from '@translations';
@@ -32,7 +32,7 @@ export const AddAccountBackup = ({ flowHeader }: IFlowComponentProps) => {
 
   return (
     <>
-      <Container>
+      <ScrollableContainer>
         {flowHeader}
         <Box sx={{ position: 'absolute', top: '-1000%', left: '-1000%' }}>
           <PaperWallet
@@ -69,19 +69,21 @@ export const AddAccountBackup = ({ flowHeader }: IFlowComponentProps) => {
             </Body>
           </>
         )}
-        <Panel mb="2">
-          <Body fontSize="14px" mb="1">
-            {translate('ADDRESS')}
-          </Body>
-          <BlockieAddress address={accounts[0].address} mb="2" />
-          {accounts[0].walletType === WalletType.MNEMONIC && (
-            <Body fontSize="14px">
-              {translate('DERIVATION_PATH')} {getFullPath(accounts[0].path, accounts[0].index)}
+        {accounts.map((account) => (
+          <Panel mb="2">
+            <Body fontSize="14px" mb="1">
+              {translate('ADDRESS')}
             </Body>
-          )}
-        </Panel>
-      </Container>
-      <PanelBottom variant="clear">
+            <BlockieAddress address={account.address} mb="2" />
+            {account.walletType === WalletType.MNEMONIC && (
+              <Body fontSize="14px">
+                {translate('DERIVATION_PATH')} {getFullPath(account.path, account.index)}
+              </Body>
+            )}
+          </Panel>
+        ))}
+      </ScrollableContainer>
+      <PanelBottom>
         <Link
           data-testid="download-link"
           href={paperWalletImage}
