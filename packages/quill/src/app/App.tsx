7ab01f0c9916d629b1c213261da9819f86ddf229
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-11-11 12:41:47 +0100

    Update Quill branding

--- packages/quill/src/app/App.tsx
+++ packages/quill/src/app/App.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/lato/300.css';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
 import '@fontsource/roboto-mono/400.css';
 
 import {
