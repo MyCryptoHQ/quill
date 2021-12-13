dd1fb718a915e40ee9d81b8cdc1e2981766e9b00
Frederik Bolding <frederik.bolding@gmail.com>
2021-12-13 15:46:05 +0100

    Try to fix build after package rename

--- packages/quill/forge.config.js
+++ packages/quill/forge.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-  packagerConfig: process.env.SHOULD_SIGN && {
-    osxSign: {
-      identity: 'Developer ID Application: MyCrypto Inc (43RN6VJTB7)',
-      'hardened-runtime': true,
-      'gatekeeper-assess': false,
-      entitlements: 'entitlements.plist',
-      'entitlements-inherit': 'entitlements.plist',
-      'signature-flags': 'library'
-    },
-    osxNotarize: {
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_ID_PASSWORD
-    },
+  packagerConfig: {
+    ...(process.env.SHOULD_SIGN
+      ? {
+          osxSign: {
+            identity: 'Developer ID Application: MyCrypto Inc (43RN6VJTB7)',
+            'hardened-runtime': true,
+            'gatekeeper-assess': false,
+            entitlements: 'entitlements.plist',
+            'entitlements-inherit': 'entitlements.plist',
+            'signature-flags': 'library'
+          },
+          osxNotarize: {
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_ID_PASSWORD
+          }
+        }
+      : {}),
+    name: 'quill',
+    executableName: 'quill',
     packageManager: 'yarn'
   },
   makers: [
