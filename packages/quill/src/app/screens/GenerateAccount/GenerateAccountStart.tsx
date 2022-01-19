bd07c4e54617e71785278ba281ef8f221b5dcebf
Frederik Bolding <frederik.bolding@gmail.com>
2022-01-19 12:31:59 +0100

    Simplify GenerateAccountStart

--- packages/quill/src/app/screens/GenerateAccount/GenerateAccountStart.tsx
+++ packages/quill/src/app/screens/GenerateAccount/GenerateAccountStart.tsx
@@ -1,9 +1,10 @@
-import { Button, Heading } from '@mycrypto/ui';
+import { Body, Button, Heading } from '@mycrypto/ui';
 import { translateRaw } from '@quill/common';
 
 import mnemonicPhrase from '@app/assets/icons/mnemonic-phrase.svg';
 import type { IFlowComponentProps } from '@components';
 import { Box, Container, Image, List, ListItem, PanelBottom } from '@components';
+import { translate } from '@translations';
 
 export const GenerateAccountStart = ({ onNext, flowHeader }: IFlowComponentProps) => (
   <>
@@ -13,14 +14,13 @@ export const GenerateAccountStart = ({ onNext, flowHeader }: IFlowComponentProps
         <Heading fontSize="24px" lineHeight="150%" mb="4">
           {translateRaw('GENERATE_ACCOUNT_MNEMONIC')}
         </Heading>
-        <Image alt="Mnemonic Phrase" src={mnemonicPhrase} width="100px" height="100px" mb="3" />
+        <Image alt="Mnemonic Phrase" src={mnemonicPhrase} width="62px" height="80px" mb="3" />
       </Box>
+      <Body>{translate('MNEMONIC_DESCRIPTION_1')}</Body>
       <List pl="0">
-        <ListItem>{translateRaw('MNEMONIC_DESCRIPTION_1')}</ListItem>
-        <ListItem>{translateRaw('MNEMONIC_DESCRIPTION_2')}</ListItem>
-        <ListItem>{translateRaw('MNEMONIC_DESCRIPTION_3')}</ListItem>
-        <ListItem>{translateRaw('MNEMONIC_DESCRIPTION_4')}</ListItem>
-        <ListItem>{translateRaw('MNEMONIC_DESCRIPTION_5')}</ListItem>
+        <ListItem mb="1">{translateRaw('MNEMONIC_DESCRIPTION_2')}</ListItem>
+        <ListItem mb="1">{translateRaw('MNEMONIC_DESCRIPTION_3')}</ListItem>
+        <ListItem mb="1">{translateRaw('MNEMONIC_DESCRIPTION_4')}</ListItem>
       </List>
     </Container>
     <PanelBottom variant="clear">
