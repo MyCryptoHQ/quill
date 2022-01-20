18ff907810545b6c5922fd43f3f1310c41cf01e4
Frederik Bolding <frederik.bolding@gmail.com>
2022-01-20 14:39:24 +0100

    Tweak AddAccountEnd

--- packages/quill/src/app/screens/AddAccount/AddAccountEnd.tsx
+++ packages/quill/src/app/screens/AddAccount/AddAccountEnd.tsx
@@ -1,30 +1,32 @@
-import { Body, Button, Heading } from '@mycrypto/ui';
+import { Body, Button, SubHeading } from '@mycrypto/ui';
 import { translateRaw } from '@quill/common';
 
 import type { IFlowComponentProps } from '@app/components';
 import { Box, Container, LinkApp, Logo, PanelBottom } from '@app/components';
 import { ROUTE_PATHS } from '@app/routing';
-import checkmark from '@assets/icons/circle-checkmark.svg';
 
 export const AddAccountEnd = ({ onReset }: IFlowComponentProps) => (
   <>
     <Container>
       <Box>
-        <Logo width="100px" height="100px" icon={checkmark} mx="auto" />
-        <Heading mt="4" mb="3" textAlign="center">
+        <Logo width="64px" height="64px" mx="auto" />
+        <SubHeading mt="3" mb="3" textAlign="center">
           {translateRaw('ADD_ACCOUNT_END_HEADER')}
-        </Heading>
+        </SubHeading>
         <Body mb="3">{translateRaw('ADD_ACCOUNT_END_BODY_1')}</Body>
         <Body>{translateRaw('ADD_ACCOUNT_END_BODY_2')}</Body>
       </Box>
     </Container>
     <PanelBottom variant="clear">
-      <LinkApp href={ROUTE_PATHS.HOME} width="100%" sx={{ display: 'block' }}>
-        <Button>{translateRaw('BACK_TO_HOME')}</Button>
+      <LinkApp href="https://app.mycrypto.com/add-account" width="100%" isExternal={true}>
+        <Button>{translateRaw('CONNECT_QUILL_TO_MYCRYPTO')}</Button>
       </LinkApp>
-      <Button variant="inverted" mt="2" onClick={onReset}>
+      <Button variant="inverted" mt="3" onClick={onReset}>
         {translateRaw('ADD_ANOTHER_ACCOUNT')}
       </Button>
+      <Box mt="3" sx={{ textAlign: 'center' }}>
+        <LinkApp href={ROUTE_PATHS.HOME}>{translateRaw('BACK_TO_HOME')}</LinkApp>
+      </Box>
     </PanelBottom>
   </>
 );
