9e93ae13a2ea2768ee00a9bc78d1690df9c24765
Frederik Bolding <frederik.bolding@gmail.com>
2022-01-25 17:38:23 +0100

    Shorten time elapsed text

--- packages/common/src/utils/date.ts
+++ packages/common/src/utils/date.ts
@@ -2,3 +2,19 @@ import { formatDistanceStrict } from 'date-fns';
 
 export const formatTimeDifference = (a: number, b: number = Date.now()) =>
   formatDistanceStrict(a, b, { addSuffix: true });
+
+const replacements = {
+  second: 's',
+  minute: 'm',
+  hour: 'h',
+  day: 'd',
+  week: 'w',
+  month: 'mo',
+  year: 'y'
+};
+
+export const formatTimeDifferenceShort = (a: number, b: number = Date.now()) =>
+  Object.entries(replacements).reduce(
+    (acc, [key, value]) => acc.replace(` ${key}s`, value).replace(` ${key}`, value),
+    formatTimeDifference(a, b)
+  );
