9e5d822af570d008ed8f73fcc22bf99c7f499ac0
Frederik Bolding <frederik.bolding@gmail.com>
2022-01-25 12:20:20 +0100

    Fix pagination alignment

--- packages/quill/src/app/components/Pagination.tsx
+++ packages/quill/src/app/components/Pagination.tsx
@@ -14,21 +14,25 @@ export interface PaginationProps {
 
 export const Pagination = ({ page, totalPages, onNext, onBack }: PaginationProps) => (
   <Flex variant="horizontal-center" mt="2">
-    {page > 0 && (
-      <LinkApp data-testid="pagination-back" href="#" onClick={onBack} p="2">
-        <Image src={arrow} sx={{ transform: 'rotate(180deg)' }} />
-      </LinkApp>
-    )}
+    <Flex flex="1" justifyContent="flex-end">
+      {page > 0 && (
+        <LinkApp data-testid="pagination-back" href="#" onClick={onBack} p="2">
+          <Image src={arrow} sx={{ transform: 'rotate(180deg)' }} />
+        </LinkApp>
+      )}
+    </Flex>
     <Body fontSize="1" mx="4" sx={{ textTransform: 'uppercase' }}>
       {translateRaw('PAGINATION', {
         $current: (page + 1).toString(),
         $total: totalPages.toString()
       })}
     </Body>
-    {page < totalPages - 1 && (
-      <LinkApp data-testid="pagination-next" href="#" onClick={onNext} p="2">
-        <Image src={arrow} />
-      </LinkApp>
-    )}
+    <Flex flex="1" justifyContent="flex-start">
+      {page < totalPages - 1 && (
+        <LinkApp data-testid="pagination-next" href="#" onClick={onNext} p="2">
+          <Image src={arrow} />
+        </LinkApp>
+      )}
+    </Flex>
   </Flex>
 );
