e7f289338eaa8e68832bef08b617b56a229dcfcc
Frederik Bolding <frederik.bolding@gmail.com>
2022-02-01 13:44:09 +0100

    Fix types

--- packages/quill/src/app/components/MnemonicInput.tsx
+++ packages/quill/src/app/components/MnemonicInput.tsx
@@ -1,5 +1,8 @@
+import type { Input } from '@mycrypto/ui';
 import { Box, Image } from '@mycrypto/ui';
+import type { ComponentProps } from 'react';
 import { useState } from 'react';
+import type { DefaultError, DefaultState, FormInputProps } from 'typed-react-form';
 
 import hide from '@assets/icons/eye-hide.svg';
 import show from '@assets/icons/eye-show.svg';
@@ -7,7 +10,15 @@ import show from '@assets/icons/eye-show.svg';
 import { FormInput } from './FormInput';
 import { FormTextArea } from './FormTextArea';
 
-export const MnemonicInput = ({ ...props }) => {
+export const MnemonicInput = <
+  T,
+  Key extends keyof T,
+  Value extends T[Key] | T[Key][keyof T[Key]],
+  State extends DefaultState = DefaultState,
+  Error extends string = DefaultError
+>({
+  ...props
+}: Omit<ComponentProps<typeof Input>, 'form'> & FormInputProps<T, State, Error, Key, Value>) => {
   const [hidden, setHidden] = useState(true);
 
   const handleToggle = () => setHidden((h) => !h);
@@ -24,6 +35,7 @@ export const MnemonicInput = ({ ...props }) => {
           sx={{ textAlign: 'right' }}
         />
       ) : (
+        // @ts-expect-error Props don't match up 100%
         <FormTextArea {...props} py="1" width="100%" pr={'2rem'} sx={{ textAlign: 'right' }} />
       )}
       <Image src={hidden ? show : hide} ml="-2rem" sx={{ zIndex: 2 }} onClick={handleToggle} />
--- packages/quill/src/app/components/PrivateKeyInput.tsx
+++ packages/quill/src/app/components/PrivateKeyInput.tsx
@@ -1,13 +1,23 @@
+import type { Input } from '@mycrypto/ui';
 import { Box, Image } from '@mycrypto/ui';
 import type { ComponentProps } from 'react';
 import { useState } from 'react';
+import type { DefaultError, DefaultState, FormInputProps } from 'typed-react-form';
 
 import hide from '@assets/icons/eye-hide.svg';
 import show from '@assets/icons/eye-show.svg';
 
 import { FormInput } from './FormInput';
 
-export const PrivateKeyInput = ({ ...props }: ComponentProps<typeof FormInput>) => {
+export const PrivateKeyInput = <
+  T,
+  Key extends keyof T,
+  Value extends T[Key] | T[Key][keyof T[Key]],
+  State extends DefaultState = DefaultState,
+  Error extends string = DefaultError
+>({
+  ...props
+}: Omit<ComponentProps<typeof Input>, 'form'> & FormInputProps<T, State, Error, Key, Value>) => {
   const [hidden, setHidden] = useState(true);
 
   const handleToggle = () => setHidden((h) => !h);
