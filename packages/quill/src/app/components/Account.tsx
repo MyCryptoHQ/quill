9a7caafedb37f63b9ef50460230cafaf1d0f90a0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-02-16 11:22:57 +0100

    Reduce code duplication

--- packages/quill/src/app/components/Account.tsx
+++ packages/quill/src/app/components/Account.tsx
@@ -18,33 +18,31 @@ export const Account = ({
   truncate: boolean;
   addressColor?: string;
   copyable?: boolean;
-} & BoxProps) => (
-  <Box bg="GREY_LIGHTEST" p="10px" variant="horizontal-start" {...props}>
-    <Blockie height="32px" width="32px" minWidth="32px" address={address} />
-    <Box pl="1">
-      <Body
-        fontSize="3"
-        sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
-      >
-        {label ?? translateRaw('NO_LABEL')}
-      </Body>
-      {copyable ? (
-        <Copyable text={address} mr="2">
-          <Body
-            fontSize="1"
-            sx={{ overflow: 'hidden', textOverflow: 'ellipsis', color: addressColor }}
-          >
-            {shouldTruncate ? truncate(address) : address}
-          </Body>
-        </Copyable>
-      ) : (
+} & BoxProps) => {
+  const account = (
+    <Body fontSize="1" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', color: addressColor }}>
+      {shouldTruncate ? truncate(address) : address}
+    </Body>
+  );
+
+  return (
+    <Box bg="GREY_LIGHTEST" p="10px" variant="horizontal-start" {...props}>
+      <Blockie height="32px" width="32px" minWidth="32px" address={address} />
+      <Box pl="1">
         <Body
-          fontSize="1"
-          sx={{ overflow: 'hidden', textOverflow: 'ellipsis', color: addressColor }}
+          fontSize="3"
+          sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
         >
-          {shouldTruncate ? truncate(address) : address}
+          {label ?? translateRaw('NO_LABEL')}
         </Body>
-      )}
+        {copyable ? (
+          <Copyable text={address} mr="2">
+            {account}
+          </Copyable>
+        ) : (
+          account
+        )}
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
